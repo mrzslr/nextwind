db3b1a77fa7268f9449fcd799f75f2e256775fee
Mohammadreza Salari <frishter.ms@gmail.com>
2020-04-22 00:39:33 +0430

    init sample :tada:

--- components/nav.js
+++ components/nav.js
@@ -23,6 +23,7 @@ export default function Nav() {
             </li>
           ))}
         </ul>
+        <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com"></input>
       </ul>
     </nav>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
+    },
+    colors: {
+      indigo: {
+        lighter: "#b3bcf5",
+        default: "#5c6ac4",
+        dark: "#202e78",
+      },
+    },
+  },
+  spacing: {
+    "1": "8px",
+    "2": "12px",
+    "3": "16px",
+    "4": "24px",
+    "5": "32px",
+    "6": "48px",
+  },
+};
